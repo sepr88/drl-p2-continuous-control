b0f7300785b4d413970164ba64f81fb53bed76b2
sepr88 <sebastian.prandl@gmail.com>
2022-05-13 10:38:04 +0200

    added leaky relu

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -25,6 +24,7 @@ class Actor(nn.Module):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fc1 = nn.Linear(state_size, fc1_units)
+        self.bn1 = nn.BatchNorm1d(fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
         self.reset_parameters()
@@ -41,10 +41,11 @@ class Actor(nn.Module):
         return torch.tanh(self.fc3(x))
 
 
+
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fcs1_units=400, fcs2_units=300):
         """Initialize parameters and build model.
         Params
         ======
@@ -52,13 +53,14 @@ class Critic(nn.Module):
             action_size (int): Dimension of each action
             seed (int): Random seed
             fcs1_units (int): Number of nodes in the first hidden layer
-            fc2_units (int): Number of nodes in the second hidden layer
+            fcs2_units (int): Number of nodes in the second hidden layer
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
         self.fcs1 = nn.Linear(state_size, fcs1_units)
-        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, 1)
+        self.bn1 = nn.BatchNorm1d(fcs1_units)
+        self.fc2 = nn.Linear(fcs1_units+action_size, fcs2_units)
+        self.fc3 = nn.Linear(fcs2_units, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -68,7 +70,7 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.relu(self.fcs1(state))
+        xs = F.leaky_relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
-        x = F.relu(self.fc2(x))
+        x = F.leaky_relu(self.fc2(x))
         return self.fc3(x)
